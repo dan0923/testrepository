d794d4ee2b8a027754ed19b931bc4da8ddfa2db0
shahrain680 <shahrin680@gmail.com>
2020-06-20 15:29:54 +0800

    locking of orientation to prevent bugs

--- app/src/main/java/com/fypic/imageclassification/Classify.java
+++ app/src/main/java/com/fypic/imageclassification/Classify.java
@@ -202,7 +202,7 @@ public class Classify extends AppCompatActivity {
             Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
             selected_image.setImageBitmap(bitmap);
             // not sure why this happens, but without this the image appears on its side
-            selected_image.setRotation(selected_image.getRotation() + 90);
+            selected_image.setRotation(selected_image.getRotation() + 0);
         } catch (IOException e) {
             e.printStackTrace();
         }
