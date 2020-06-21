7c6c6e4757c460a2afdd084b4de424160d39039e
wlcl2001 <wizliechulok@gmail.com>
2020-06-21 23:23:53 +0800

    Sort activities

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/androidx/constraintlayout/widget/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/androidx/constraintlayout/widget/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f090049;
-        public static final int end = 0x7f090078;
-        public static final int gone = 0x7f090085;
-        public static final int invisible = 0x7f090090;
-        public static final int left = 0x7f090095;
-        public static final int packed = 0x7f0900b4;
-        public static final int parent = 0x7f0900b9;
-        public static final int percent = 0x7f0900bd;
-        public static final int right = 0x7f0900c5;
-        public static final int spread = 0x7f0900e9;
-        public static final int spread_inside = 0x7f0900ea;
-        public static final int start = 0x7f0900ef;
-        public static final int top = 0x7f090115;
-        public static final int wrap = 0x7f09012a;
+        public static final int end = 0x7f090079;
+        public static final int gone = 0x7f090086;
+        public static final int invisible = 0x7f090091;
+        public static final int left = 0x7f090096;
+        public static final int packed = 0x7f0900b5;
+        public static final int parent = 0x7f0900ba;
+        public static final int percent = 0x7f0900be;
+        public static final int right = 0x7f0900c6;
+        public static final int spread = 0x7f0900ea;
+        public static final int spread_inside = 0x7f0900eb;
+        public static final int start = 0x7f0900f0;
+        public static final int top = 0x7f090116;
+        public static final int wrap = 0x7f09012b;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/com/fypic/imageclassification/ListActivity.java
+++ app/src/main/java/com/fypic/imageclassification/ListActivity.java
@@ -1,5 +1,6 @@
 package com.fypic.imageclassification;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -7,25 +8,32 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 
-public class ListActivity extends AppCompatActivity {
+public class ListActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     DatabaseHelper db;
     private ListView lv;
     int fl;
     int sl;
-    Button allBtn,paperBtn, metalBtn, plasticBtn, wasteBtn, summaryBtn;
+    String dateStr;
+    Button allBtn,paperBtn, metalBtn, plasticBtn, wasteBtn, summaryBtn, dateBtn;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +46,7 @@ public class ListActivity extends AppCompatActivity {
         plasticBtn = findViewById(R.id.plasticBtn);
         wasteBtn = findViewById(R.id.wasteBtn);
         summaryBtn = findViewById(R.id.summarybutton);
+        dateBtn = findViewById(R.id.dateBtn);
 
         Cursor data = db.getData();
         Cursor alldata = db.getAllData();
@@ -53,51 +62,51 @@ public class ListActivity extends AppCompatActivity {
         ArrayList<Object> plasticObj = new ArrayList<>();
         ArrayList<Object> wasteObj = new ArrayList<>();
 
-        while(data.moveToNext()){
+        while (data.moveToNext()) {
             fl = data.getInt(1);
             sl = data.getInt(0);
 
-            Object obj = new Object(sl,fl);
+            Object obj = new Object(sl, fl);
             listObj.add(obj);
         }
 
-        while(alldata.moveToNext()){
+        while (alldata.moveToNext()) {
             fl = alldata.getInt(1);
             sl = alldata.getInt(0);
 
-            Object obj = new Object(sl,fl);
+            Object obj = new Object(sl, fl);
             allObj.add(obj);
         }
 
-        while(paperdata.moveToNext()){
+        while (paperdata.moveToNext()) {
             fl = paperdata.getInt(1);
             sl = paperdata.getInt(0);
 
-            Object obj = new Object(sl,fl);
+            Object obj = new Object(sl, fl);
             paperObj.add(obj);
         }
 
-        while(metaldata.moveToNext()){
+        while (metaldata.moveToNext()) {
             fl = metaldata.getInt(1);
             sl = metaldata.getInt(0);
 
-            Object obj = new Object(sl,fl);
+            Object obj = new Object(sl, fl);
             metalObj.add(obj);
         }
 
-        while(plasticdata.moveToNext()){
+        while (plasticdata.moveToNext()) {
             fl = plasticdata.getInt(1);
             sl = plasticdata.getInt(0);
 
-            Object obj = new Object(sl,fl);
+            Object obj = new Object(sl, fl);
             plasticObj.add(obj);
         }
 
-        while(wastedata.moveToNext()){
+        while (wastedata.moveToNext()) {
             fl = wastedata.getInt(1);
             sl = wastedata.getInt(0);
 
-            Object obj = new Object(sl,fl);
+            Object obj = new Object(sl, fl);
             wasteObj.add(obj);
         }
 
@@ -111,12 +120,13 @@ public class ListActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 Intent editScreenIntent = new Intent(ListActivity.this, EditActivity.class);
-                editScreenIntent.putExtra("id",listObj.get(position).getId());
-                editScreenIntent.putExtra("name",listObj.get(position).getMat());
+                editScreenIntent.putExtra("id", listObj.get(position).getId());
+                editScreenIntent.putExtra("name", listObj.get(position).getMat());
                 startActivity(editScreenIntent);
                 overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
             }
         });
+
 
         allBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -163,14 +173,32 @@ public class ListActivity extends AppCompatActivity {
             }
         });
 
+        dateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "date picker");
+            }
+        });
+
         summaryBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(ListActivity.this, SummaryActivity.class);
+                Intent intent = new Intent(ListActivity.this, SummaryActivity.class);
                 startActivity(intent);
                 overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
             }
         });
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String currentDateString = DateFormat.getDateInstance(DateFormat.SHORT, Locale.JAPAN).format(c.getTime());
+        dateStr = currentDateString.replace("/","-");;
     }
 
     @Override
